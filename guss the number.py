bce4f641e4f2c90eb1ef1c365d80543944358ccf
Aswathi307 <aswathi.24.beai@acharya.ac.in>
2025-05-22 09:01:46 +0530

    Create guess the number.py

---
+++ guss the number.py
@@ -0,0 +1,28 @@
+print("name:Aswathi.TP\n usn :1AY24AI109\n sec:O") 
+import random
+
+def guess_the_number():
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+
+    print("Welcome to 'Guess the Number'!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if guess < number_to_guess:
+                print("Too low! Try again.")
+            elif guess > number_to_guess:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Please enter a valid number.")
+
+# Run the game
+guess_the_number()
+p
