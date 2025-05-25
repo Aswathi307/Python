df05339f3785e9549ef7304309e3b366747f5ac6
Aswathi307 <aswathi.24.beai@acharya.ac.in>
2025-05-25 15:21:25 +0530

    Update CoinFlipStreaks.py

--- CoinFlipStreaks.py
+++ CoinFlipStreaks.py
@@ -4,8 +4,7 @@ import random
 def coin_flip_streaks(num_flips=10000, streak_length=6):
     flips = []
     for _ in range(num_flips):
-        flips.append(random.choice(['H', 'T']))  # H for heads, T for tails
-
+        flips.append(random.choice(['H', 'T'])) 
     streaks = 0
     current_streak = 1
 
@@ -18,7 +17,4 @@ def coin_flip_streaks(num_flips=10000, streak_length=6):
             current_streak = 1
 
     print(f'Number of streaks of {streak_length} or more: {streaks}')
-    # Optional: Uncomment to see the flip results
-    # print(''.join(flips))
-
 coin_flip_streaks()
