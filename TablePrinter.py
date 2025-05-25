260884232e246282c0c2cf5bba1b2c40e3b4a933
Aswathi307 <aswathi.24.beai@acharya.ac.in>
2025-05-25 15:29:39 +0530

    Update and rename table printer.py to TablePrinter.py

--- TablePrinter.py
+++ TablePrinter.py
@@ -1,17 +1,12 @@
-print("name:Aswathi\nsec:o\nusn:1AY24AI109")
+print("name:T P Aswathi\nsec:o\nusn:1AY24AI109")
 def print_table(table_data):
    col_widths = [max(len(str(item)) for item in col) for col in table_data]
-
-    # Find number of rows (assuming all columns are equal in length)
     num_rows = len(table_data[0])
 
     for row_idx in range(num_rows):
         for col_idx in range(len(table_data)):
             print(str(table_data[col_idx][row_idx]).rjust(col_widths[col_idx]), end=' ')
-        print()  # Newline after each row
-
-
-# Example table data
+        print() 
 table = [
     ['apples', 'oranges', 'cherries', 'banana'],
     ['Alice', 'Bob', 'Carol', 'David'],
