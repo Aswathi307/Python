57df6b033200276dfb6c6b060562668479a30d35
Aswathi307 <aswathi.24.beai@acharya.ac.in>
2025-05-25 18:02:06 +0530

    Update CollatzSequence.py

--- CollatzSequence.py
+++ CollatzSequence.py
@@ -1,20 +1,17 @@
 print("name : T P Aswathi,usn :1AY24AI109,sec:O")
 def collatz_sequence(n):
-    if n <= 0:
+    if n < 1:
         print("Please enter a positive integer.")
         return
 
-    print("Collatz sequence:")
-    print(n, end=' ')
     while n != 1:
-        if n % 2 == 0: 
+        print(n, end=" -> ")
+        if n % 2 == 0:
             n = n // 2
-        else:          
+        else:
             n = 3 * n + 1
-        print(n, end=' ')
-    print() 
-try:
-    num = int(input("Enter a positive integer: "))
-    collatz_sequence(num)
-except ValueError:
-    print("Invalid input. Please enter an integer.")
+    print(1)  # Final value
+
+# Example usage
+num = int(input("Enter a positive integer: "))
+collatz_sequence(num)
