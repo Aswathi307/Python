2bee1a26fdae8aa7f3d90690abd210a401f105e6
Aswathi307 <aswathi.24.beai@acharya.ac.in>
2025-05-25 15:31:39 +0530

    Update zigzag.py

--- zigzag.py
+++ zigzag.py
@@ -1,6 +1,5 @@
-print("Name:T P Aswathi,usn :1AY24AI109,sec:O
+print("Name:T P Aswathi\nsec:O\nusn :1AY24AI109")
 def zigzag_pattern(n):
-    # n: number of columns
     for row in range(3):  # Fixed 3 rows for basic zigzag
         for col in range(n):
             if (row + col) % 4 == 0 or (row == 1 and col % 4 == 0):
@@ -8,6 +7,4 @@ def zigzag_pattern(n):
             else:
                 print(" ", end="")
         print()
-
-# Example: zigzag of length 20
 zigzag_pattern(20)
