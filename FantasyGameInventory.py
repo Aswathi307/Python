485cf0250d876ab01ca83dc375c5d49ce8c97e18
Aswathi307 <aswathi.24.beai@acharya.ac.in>
2025-05-25 15:45:38 +0530

    Update FantasyGameInventory.py

--- FantasyGameInventory.py
+++ FantasyGameInventory.py
@@ -1,24 +1,33 @@
 print("name:T P Aswathi\nsec:o\nusn:1AY24AI109")
 def display_inventory(inventory):
     print("Inventory:")
-    total_items = 0
+    item_total = 0
     for item, count in inventory.items():
         print(f"{count} {item}")
-        total_items += count
-    print(f"Total number of items: {total_items}\n")
+        item_total += count
+    print(f"Total number of items: {item_total}")
+
 def add_to_inventory(inventory, added_items):
     for item in added_items:
         inventory[item] = inventory.get(item, 0) + 1
     return inventory
-player_inventory = {
-    'rope': 1,
-    'torch': 6,
-    'gold coin': 42,
-    'dagger': 1
-}
-dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-print("Before loot:")
-display_inventory(player_inventory)
-o_inventory(player_inventory, dragon_loot)
-print("After loot:")
-display_inventory(player_inventory)
+def main():
+    inventory = {
+        'gold coin': 42,
+        'rope': 1,
+        'dagger': 1,
+        'torch': 6,
+        'arrow': 12
+    }
+    dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+    print("Before collecting loot:")
+    display_inventory(inventory)
+
+    print("\nCollecting loot...")
+    inventory = add_to_inventory(inventory, dragon_loot)
+
+    print("\nAfter collecting loot:")
+    display_inventory(inventory)
+if __name__ == "__main__":
+    main()
